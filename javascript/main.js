66d1cc82f33ef641032b55197967c05e6a0fb198
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2020-05-08 17:56:47 -0300

    modificaciones de la clase y renombre variables

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,26 @@
 "use strict";
 let recetas_dulce = ["panqueques", "flan", "torta frita","torta de manzana","buñuelos","torta de ricota"];
 let recetas_salada = [ "tortilla", "tarta de choclo","buñuelos de espinaca ","hamburguesas de lentejas" , "mac and chese","albondigas"];
-let recetas = document.getElementById("receta_salada");
-recetas.addEventListener("click", mostrar_receta_salada);
-let recetass= document.getElementById("receta_dulce");
-recetass.addEventListener("click", mostrar_receta_dulce);
+let recetasalada = document.getElementById("receta_salada");
+recetasalada.addEventListener("click", mostrar_receta_salada);
+let recetadulce= document.getElementById("receta_dulce");
+recetadulce.addEventListener("click", mostrar_receta_dulce);
 
 
-function click_salado(){ 
-    if (document.formulario_random.Check1.checked === true){
-    
-    } 
-    return true ;
-} 
-function click_dulce(){ 
-    if (document.formulario_random.Check1.checked === true){
-        
-    } 
-    return true;
-} 
 
-function dulce(){
-    let max_dulce = recetas_dulce.length;
+function dulce(max_dulce){
     let random_dulce = Math.floor(Math.random() * max_dulce);
     return random_dulce;
 }
 
-function salado(){
-    let max_salada = recetas_salada.length;
+function salado(max_salada){
     let random_salada = Math.floor(Math.random() * max_salada);
     return random_salada;
 }
 
 function obtener_receta_salada(){
     let receta_elegida_salada;
-  if ( click_salado() === true ){
+  if (recetasalada.checked){
     receta_elegida_salada  = recetas_salada[salado(recetas_salada.length)];
   }
     return receta_elegida_salada;
@@ -42,7 +28,7 @@ function obtener_receta_salada(){
 
   function obtener_receta_dulce(){
     let receta_elegida_dulce;
-   if( click_dulce()=== true ){
+   if(  recetadulce.checked ){
     receta_elegida_dulce = recetas_dulce[dulce(recetas_dulce.length)];
  }
     return receta_elegida_dulce;

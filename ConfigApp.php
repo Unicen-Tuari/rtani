40f9cc225365709cae66687128a592212a0cecfd
Rosario Tani Gomez <ro.tani.gomez@gmail.com>
2020-06-07 13:13:10 -0300

    Config

---
+++ ConfigApp.php
@@ -0,0 +1,14 @@
+<?php
+
+class ConfigApp{
+    public static $ACTION = "action";
+    public static $PARAMS = "params";
+    public static $ACTIONS = [
+        'home' => 'admin_recipes',
+        'insertarReceta' => 'insertarReceta',
+        'insert'=> 'Agregar_receta',
+        'borrarReceta' => 'borrarReceta',
+        'finalizar' => 'finalizar',
+    ];
+}
+?>
